28988679bda6f2c01e0889b47510a63450203ac7
Jaeryong Chae <jr_chae@naver.com>
2016-05-31 14:36:42 +0900

    SampleApp Store

---
+++ SampleApp/SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  secondViewController.swift
+//  SampleApp
+//
+//  Created by MF839-008 on 2016. 5. 31..
+//  Copyright © 2016년 JRChae. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func close(sender: AnyObject) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    @IBOutlet weak var myLable: UILabel! //암묵적 Optional
+    
+    @IBAction func openSecondView(sender: AnyObject) {
+        myLable.text = "Test"
+        
+        // 화면전환을 위해 segue 연결
+        performSegueWithIdentifier("thirdsegue", sender: self)
+    }
+    
+    @IBAction func openLoginView(sender: AnyObject) {
+        // strotyboard에서 Identifier를 이용하여 가져와서 Instance로 만든다.
+        if let loginView = self.storyboard?.instantiateViewControllerWithIdentifier("loginview") {
+            
+            loginView.modalTransitionStyle = UIModalTransitionStyle.CoverVertical
+            self.presentViewController(loginView, animated: true, completion: nil)
+        }
+    }
+    
+    override func viewDidLoad() { // override : UIViewController내의 func를 override
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //print("viewDidLoad")
     }
 
     override func didReceiveMemoryWarning() {
